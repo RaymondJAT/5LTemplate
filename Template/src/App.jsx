4645ae81d07d46794190f449bddaf567d8bfe95b
RaymondJAT <rjat081700@gmail.com>
2025-01-25 14:47:57 +0800

    first commit

---
+++ Template/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Navigationbar from "./components/Navigationbar";
+
+function App() {
+  return (
+    <>
+      <Navigationbar />
+    </>
+  );
+}
+
+export default App;
---
+++ Template/src/components/Navigationbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navigationbar = () => {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold text-center text-blue-400 bg-red-500">
+        New React Project
+      </h1>
+    </div>
+  );
+};
+
+export default Navigationbar;
